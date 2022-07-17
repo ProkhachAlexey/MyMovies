8ab9efc93abc52606ad5a35e5d04607eadad0aaa
Alexey Prokhach <asprokhach68@gmail.com>
2022-07-18 00:01:33 +0300

    part2 commit

--- app/src/main/java/com/prokhach/mymovies/MainActivity.java
+++ app/src/main/java/com/prokhach/mymovies/MainActivity.java
@@ -1,6 +1,8 @@
 package com.prokhach.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -16,16 +18,21 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerViewPosters;
+    private MovieAdapter movieAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        movieAdapter = new MovieAdapter();
+
         JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        StringBuilder builder = new StringBuilder();
-        for (Movie movie : movies) {
-            builder.append(movie.getTitle()).append("\n");
-        }
-        Log.d("MainActivity", builder.toString());
+        movieAdapter.setMovies(movies);
+        recyclerViewPosters.setAdapter(movieAdapter);
     }
 }
