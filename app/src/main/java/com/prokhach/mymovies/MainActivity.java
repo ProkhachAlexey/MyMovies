a48c68362c8c8c94839153f846b9726f1a9b52b4
Alexey Prokhach <asprokhach68@gmail.com>
2022-07-18 20:58:34 +0300

    part3 commit

--- app/src/main/java/com/prokhach/mymovies/MainActivity.java
+++ app/src/main/java/com/prokhach/mymovies/MainActivity.java
@@ -6,8 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.prokhach.mymovies.data.Movie;
 import com.prokhach.mymovies.utils.JSONUtils;
 import com.prokhach.mymovies.utils.NetworkUtils;
@@ -18,6 +23,9 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SwitchMaterial switchSort;
+    private TextView textViewPopularity;
+    private TextView textViewTopRated;
     private RecyclerView recyclerViewPosters;
     private MovieAdapter movieAdapter;
 
@@ -26,13 +34,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        switchSort = findViewById(R.id.switchSort);
+        textViewPopularity = findViewById(R.id.textViewPopularity);
+        textViewTopRated = findViewById(R.id.textViewTopRated);
         recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
         recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
         movieAdapter = new MovieAdapter();
+        recyclerViewPosters.setAdapter(movieAdapter);
+        switchSort.setChecked(true);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMethodOfSort(isChecked);
+            }
+        });
+        switchSort.setChecked(false);
 
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
+        textViewPopularity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMethodOfSort(false);
+                switchSort.setChecked(false);
+            }
+        });
+
+        textViewTopRated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setMethodOfSort(true);
+                switchSort.setChecked(true);
+            }
+        });
+
+        movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Toast.makeText(MainActivity.this, "Poster №" + (position + 1), Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        movieAdapter.setOnReachEndListener(new MovieAdapter.OnReachEndListener() {
+            @Override
+            public void onReachEnd() {
+                Toast.makeText(MainActivity.this, "Конец списка", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setMethodOfSort(boolean isTopRated) {
+        int methodOfSort;
+        if (isTopRated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            textViewTopRated.setTextColor(getResources().getColor(R.color.teal_200));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.white));
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+            textViewTopRated.setTextColor(getResources().getColor(R.color.white));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.teal_200));
+        }
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
         movieAdapter.setMovies(movies);
-        recyclerViewPosters.setAdapter(movieAdapter);
     }
 }
